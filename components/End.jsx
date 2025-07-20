a1f318670ae293b33bb071c9deb14322b4732f54
hurlx <n247684@gmail.com>
2025-07-20 06:37:44 +0300

    Update End.jsx

--- components/End.jsx
+++ components/End.jsx
@@ -33,13 +33,13 @@ export default function GlassMorphCard({ product }) {
 					/>
 
 					{/* Glass Morph Info */}
-					<div className="absolute bottom-0 w-full p-5 bg-white/30 backdrop-blur-lg text-white">
+					<div className="absolute bottom-0 w-full p-5 bg-white/30 backdrop-blur-lg text-gray-500">
 						<h3 className="text-lg font-semibold drop-shadow">{product.name}</h3>
-						<p className="text-sm text-white/80 mb-3">
+						<p className="text-sm text-gray-500 mb-3">
 							{product.shortDescription}
 						</p>
 						<div className="flex justify-between items-center">
-						<span className="text-md font-bold">ل س{product.price}</span>
+						<span className="text-md font-bold">$ {product.price}</span>
 					</div>
 					</div>
 				</div>
