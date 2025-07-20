ad3eb9993555e0140590c07f512679fbb1805ba9
hurlx <n247684@gmail.com>
2025-07-20 14:42:54 +0300

    Update data.jsx

--- app/Lib/data.jsx
+++ app/Lib/data.jsx
@@ -5,13 +5,12 @@ export const products = [
     image: "/images/1/Kaki.jpg",
     hoverImage: "/images/1/Coffee.jpg",
     price: 20,
-    tag: "Limited",
     id: "1",
     slug: "gg",
     images: [
       "/images/1/Coffee.jpg",
       "/images/1/Brown.jpg",
-      "/images/1/Kaki.jpg"
+      "/images/1/Kaki.jpg",
     ],
     colors: [
       { name: "Coffee", hex: "#6F4E37" },
@@ -25,7 +24,6 @@ export const products = [
     image: "/images/2/Kaki.jpg",
     hoverImage: "/images/2/White.jpg",
     price: 17,
-    tag: "Limited",
     id: "2",
     slug: "aa",
     images: [
@@ -38,7 +36,28 @@ export const products = [
       { name: "White", hex: "#ffffff" },
       { name: "Black", hex: "#000000" },
       { name: "Wheat", hex: "#f5deb3" },
-      { name: "LigtGreen", hex: "#90EE90" }
+      { name: "Phosphorescent", hex: "#90EE90" }
+    ],
+  },
+  {
+    name: "حقيبة القش",
+    shortDescription: "حقيبة كروس أنيقة مصنوعة من القش الطبيعي مع غطاء جلدي ، بتفاصيل مضفّرة ولمسة عصرية من الشرّابات والحبل القطني.",
+    image: "/images/4/Black.jpg",
+    hoverImage: "/images/4/White.jpg",
+    price: 22,
+    id: "3",
+    slug: "ab",
+    images: [
+      "/images/4/Black.jpg",
+      "/images/4/Brown.jpg",
+      "/images/4/White.jpg",
+      "/images/4/Yellow.jpg"
+    ],
+    colors: [
+      { name: "White", hex: "#ffffff" },
+      { name: "Black", hex: "#000000" },
+      { name: "Brown", hex: "#964B00" },
+      { name: "Yellow", hex: "#FFFF00" }
     ],
   },
 ];
